9c5e5ea9f0dadae4edeed1e95d312f04548eaf6f
maria <alecornejolupa2013@gmail.com>
2020-01-10 23:56:22 -0500

    folder error

--- README.md
+++
@@ -1,2 +0,0 @@
-# DCPD-CloudComputing
-Here is a distributed version of Compresed Path Database
--- upload-storage.sh
+++
@@ -1,2 +0,0 @@
-BUCKET=dataproc-ed3c3d29-fb10-47bb-aca7-dcc358c68973-us-central1
-gsutil -m cp -r ./storage/* gs://$BUCKET/
